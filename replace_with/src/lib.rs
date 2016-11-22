20aeb6c7455c01d1a9febae02cd61045434c7906
rmawhorter <rmawhorter@g.hmc.edu>
2016-11-22 01:50:07 -0800

    fixed a thing where I ptr::wrote to a mutable reference

--- replace_with/src/lib.rs
+++ replace_with/src/lib.rs
@@ -5,6 +5,6 @@ pub fn replace_with<T, F>(t: &mut T, f: F) where F: FnOnce(T) -> T
 	unsafe
 	{
 		let t_ptr = t as *mut T;
-		ptr::write(t, f(ptr::read(t_ptr)));
+		ptr::write(t_ptr, f(ptr::read(t_ptr)));
 	}	
 }
