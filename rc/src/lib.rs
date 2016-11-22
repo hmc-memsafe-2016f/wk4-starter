2c1adf0eca4d917d30a714ae0e77901346bd728d
Michael Sheely <msheely@hmc.edu>
2016-11-22 15:49:50 -0800

    Added implementation of reference counters

--- rc/src/lib.rs
+++ rc/src/lib.rs
@@ -1,29 +1,61 @@
-
 use std::ops::Deref;
+use std::ptr;
 
-struct MyRC<T> {
-  data: Box<T>
+pub struct MyRc<T> {
+  data: *mut T,
+  count: *mut usize,
 }
 
-impl<T> MyRC<T> {
-  fn new(t: T) -> MyRC<T> {
-      MyRC{data: Box::new(t)}
+impl<T> MyRc<T> {
+  pub fn new(t: T) -> MyRc<T> {
+      let data_box  = Box::new(t);
+      let count_box = Box::new(1);  // one reference to the underlying data
+      let data_ptr  = Box::into_raw(data_box);  // now we own the heap data
+      let count_ptr = Box::into_raw(count_box);
+      MyRc{data: data_ptr, count: count_ptr}
   }
 
-  fn consume(self) -> Result<T, MyRC<T> > {
-      Ok(*self.data)
+  pub fn consume(self) -> Result<T, MyRc<T> > {
+    unsafe {
+      if *self.count == 1 {
+        let t: T = ptr::read(self.data);
+        Ok(t)
+      } else {
+        Err(self)
+      }
+    }
   }
+
 }
 
-impl<T> Deref for MyRC<T> {
+impl<T> Deref for MyRc<T> {
     type Target = T;
     fn deref(&self) -> &T {
-        &self.data
+        unsafe {
+          &(*self.data)
+        }
     }
 }
-impl<T> Clone for MyRC<T> {
-    fn clone(&self) -> MyRC<T> {
-        MyRC{data: self.data}
+
+impl<T> Clone for MyRc<T> {
+    fn clone(&self) -> MyRc<T> {
+        unsafe {
+          *self.count += 1;  // there is now another reference
+          MyRc{data: self.data, count: self.count}
+        }
     }
+}
+
+impl<T> Drop for MyRc<T> {
+  fn drop(&mut self) {
+    unsafe {
+      if *self.count == 1 {
+        let boxed_data = Box::from_raw(self.data);
+        let boxed_counter = Box::from_raw(self.count);
+      } else {
+        *self.count -= 1;
+      }
+    }
+  }
 }
 
